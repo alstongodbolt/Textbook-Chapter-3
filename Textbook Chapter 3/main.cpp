bfa19484a92eceea40ec8706d9dd7edc77506a01
Alston Godbolt <alstongodbolt@Alstons-MacBook-Air.local>
2015-09-02 14:24:47 -0500

    Initial Commit

---
+++ Textbook Chapter 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Textbook Chapter 3
+//
+//  Created by Alston Godbolt on 9/2/15.
+//  Copyright (c) 2015 Alston Godbolt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
